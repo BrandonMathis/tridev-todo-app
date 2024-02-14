24ba49dfcef2b0d4350adcb41f9d9e3fa34c0d56
Brandon Mathis <bemathis@gmail.com>
2024-02-14 10:52:54 -0500

    Nav between pages

--- app/javascript/components/AllTodoLists.jsx
+++ app/javascript/components/AllTodoLists.jsx
@@ -1,7 +1,11 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
 export default function AllTodoLists() {
   return (
     <div>
       <h1>TODO Lists</h1>
+      <NavLink to="/todos/1">List 1</NavLink>
     </div>
   );
 }
--- app/javascript/components/TodoList.jsx
+++ app/javascript/components/TodoList.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { NavLink } from "react-router-dom";
 
 export default function TodoList() {
   return (
     <div>
       <h1>TODO List</h1>
+      <NavLink to="/">&lt; Back</NavLink>
     </div>
   );
 }
